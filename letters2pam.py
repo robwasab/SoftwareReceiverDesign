8f4c387c28509a3e10b26dcc79783378f04cb996
Robby Tong <robwasab@gmail.com>
2016-07-25 23:10:41 -0700

    making sure everything is up to date

--- letters2pam.py
+++ letters2pam.py
@@ -4,6 +4,58 @@ import itertools
 def letters2pam(s):
 	return list(itertools.chain(*[[int(pair[0] + pair[1],2)*2-3 for pair in np.reshape(list('{:08b}'.format(ord(c))),(4,2))]  for c in s]))
 
+def educational():
+	s = 'dead'
+	print ['{:08b}'.format(ord(c)) for c in s ]
+	# >>
+	# ['01100100', '01100101', '01100001', '01100100']
+
+	print [list('{:08b}'.format(ord(c))) for c in s ]
+	# >>
+	# [['0', '1', '1', '0', '0', '1', '0', '0'], 
+	#  ['0', '1', '1', '0', '0', '1', '0', '1'], 
+	#  ['0', '1', '1', '0', '0', '0', '0', '1'], 
+	#  ['0', '1', '1', '0', '0', '1', '0', '0']]
+	
+	print [np.reshape(list('{:08b}'.format(ord(c))),(4,2)).tolist() for c in s ]
+	# >>
+	# [[['0', '1'], 
+	#   ['1', '0'], 
+	#   ['0', '1'], 
+	#   ['0', '0']], 
+	#  [['0', '1'], 
+	#   ['1', '0'],
+	#   ['0', '1'], 
+	#   ['0', '1']], 
+	#  [['0', '1'], 
+	#   ['1', '0'], 
+	#   ['0', '0'], 
+	#   ['0', '1']], 
+	#  [['0', '1'], 
+	#   ['1', '0'], 
+	#   ['0', '1'], 
+	#   ['0', '0']]]
+
+	print [[''.join(pair) for pair in np.reshape(list('{:08b}'.format(ord(c))),(4,2)).tolist()] for c in s ]
+	# >>
+	# [['01', '10', '01', '00'], ['01', '10', '01', '01'], ['01', '10', '00', '01'], ['01', '10', '01', '00']]
+	
+	# convert each bit pair to a decimal number, these will represent amplitudes
+	print [[int(''.join(pair),2) for pair in np.reshape(list('{:08b}'.format(ord(c))),(4,2)).tolist()] for c in s ]
+	# >>
+	# [[1, 2, 1, 0], [1, 2, 1, 1], [1, 2, 0, 1], [1, 2, 1, 0]]
+
+	# shift and scale each number to -3, -1, 1, 3
+	print [[int(''.join(pair),2)*2-3 for pair in np.reshape(list('{:08b}'.format(ord(c))),(4,2)).tolist()] for c in s ]
+	# >>
+	# [[-1, 1, -1, -3], [-1, 1, -1, -1], [-1, 1, -3, -1], [-1, 1, -1, -3]]
+
+	# combine all the elements into one list with itertools
+	import itertools
+	print list(itertools.chain(*[[int(''.join(pair),2)*2-3 for pair in np.reshape(list('{:08b}'.format(ord(c))),(4,2)).tolist()] for c in s ]))
+	# >>
+	# [-1, 1, -1, -3, -1, 1, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3]
+	
 def test():
 	tests ={'dead': [-1, 1, -1, -3, -1, 1, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3], \
 		'DEAD': [-1, -3, -1, -3, -1, -3, -1, -1, -1, -3, -3, -1, -1, -3, -1, -3],\
@@ -16,5 +68,6 @@ def test():
 		except Exception as e:
 			print test
 			raise e
-if __name__ == '__name__':
+if __name__ == '__main__':
 	test()
+	educational()
